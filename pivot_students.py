5c30ba0f78648d01fda612b34c333aca725aeae5
Spyropoulos-Nikolaos <nikos.millwall@gmail.com>
2026-03-01 22:23:51 +0200

    Update pivot_students.py

--- pivot_students.py
+++ pivot_students.py
@@ -1,3 +1,16 @@
+"""
+Pivot Transformation: Student Grades
+
+This script demonstrates a pivot operation by reorganizing the dataset.
+
+Original structure:
+student -> list of grades
+
+Transformed structure:
+grade -> list of students who achieved that grade
+
+This is a fundamental data analysis operation used for grouping and restructuring data.
+"""
 students = {
     "nikos": [17, 18, 16],
     "maria": [15, 19, 14],
